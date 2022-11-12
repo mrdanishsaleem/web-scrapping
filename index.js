af4118704ba2b4042c7e446c66c3e266fb99624c
Danish Saleem <danish_s@outlook.com>
2022-11-12 22:52:21 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -1,23 +1,58 @@
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://traversymedia.com");
+  await page.goto("https://www.traversymedia.com");
 
-  await page.screenshot({ path: "example.png", fullPage: true });
+  //  Get a screenshot of the page
+  // await page.screenshot({ path: 'example.png', fullPage: true });
 
-  await page.pdf({ path: "example.pdf", format: "A4" });
+  //  Get a PDF of the page
+  // await page.pdf({ path: 'example.pdf', format: 'A4' });
 
-  const html = await page.content();
+  //  Get HTML of the page
+  // const html = await page.content();
 
-  const title = await page.evaluate(() => document.title);
+  //  Get text of the page
+  // const title = await page.evaluate(() => document.title);
 
-  const text = await page.evaluate(() => document.body.innerText);
+  //  Get text of the page
+  // const text = await page.evaluate(() => document.body.innerText);
 
-  const links = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("#courses .card"), (e) => ({}))
+  //  Get all links
+  // const links = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll('a'), (e) => e.href)
+  // );
+
+  //  Get courses
+  // const courses = await page.evaluate(() =>
+  //   Array.from(document.querySelectorAll('#courses .card'), (e) => ({
+  //     title: e.querySelector('.card-body h3').innerText,
+  //     level: e.querySelector('.card-body .level').innerText,
+  //     url: e.querySelector('.card-footer a').href,
+  //     promo: e.querySelector('.card-footer .promo-code .promo').innerText,
+  //   }))
+  // );
+
+  // Get courses using $$eval
+  const courses = await page.$$eval("#courses .card", (elements) =>
+    elements.map((e) => ({
+      title: e.querySelector(".card-body h3").innerText,
+      level: e.querySelector(".card-body .level").innerText,
+      url: e.querySelector(".card-footer a").href,
+      promo: e.querySelector(".card-footer .promo-code .promo").innerText,
+    }))
   );
+
+  console.log(courses);
+
+  // Save data to JSON file
+  fs.writeFile("courses.json", JSON.stringify(courses), (err) => {
+    if (err) throw err;
+    console.log("File saved");
+  });
 
   await browser.close();
 }
