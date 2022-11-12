f5e9fb2791a6630e758fcbb44f5d56fd2b489189
Danish Saleem <danish_s@outlook.com>
2022-11-12 19:44:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require("puppeteer");
 async function run() {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://google.com.pk/");
+  await page.goto("https://traversymedia.com");
 
   //   await page.screenshot({ path: "example.png", fullPage: true });
 
@@ -14,11 +14,10 @@ async function run() {
   //   const title = await page.evaluate(() => document.title);
 
   //   const text = await page.evaluate(() => document.body.innerText);
-  const links = await page.evaluate(() =>
-    Array.from(document.querySelectorAll("a"), (e) => e.href)
-  );
 
-  console.log(links);
+  const links = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("#courses .card"), (e) => ({}))
+  );
 
   await browser.close();
 }
