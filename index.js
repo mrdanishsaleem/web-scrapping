d95b340ae34d4fd5e43363b33f60c7e8d781c8cc
Danish Saleem <danish_s@outlook.com>
2022-11-12 15:45:18 +0500

    Create files
    - index.js
    - example.pdf
    - example.png

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ async function run() {
   const page = await browser.newPage();
   await page.goto("https://traversymedia.com");
 
-  await page.screenshot({ path: "example.png", fullPage: true });
-  await page.pdf({ path: "example.pdf", format: "A4" });
+  //   await page.screenshot({ path: "example.png", fullPage: true });
+  //   await page.pdf({ path: "example.pdf", format: "A4" });
+
+  const html = await page.content();
+  console.log(html);
 
   await browser.close();
 }
