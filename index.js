50b5c8c2a91d30c8bd3708779e407c598fb11443
Danish Saleem <danish_s@outlook.com>
2022-11-12 22:11:02 +0500

    Uncommented the code

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ async function run() {
   const page = await browser.newPage();
   await page.goto("https://traversymedia.com");
 
-  //   await page.screenshot({ path: "example.png", fullPage: true });
+  await page.screenshot({ path: "example.png", fullPage: true });
 
-  //   await page.pdf({ path: "example.pdf", format: "A4" });
+  await page.pdf({ path: "example.pdf", format: "A4" });
 
-  //   const html = await page.content();
+  const html = await page.content();
 
-  //   const title = await page.evaluate(() => document.title);
+  const title = await page.evaluate(() => document.title);
 
-  //   const text = await page.evaluate(() => document.body.innerText);
+  const text = await page.evaluate(() => document.body.innerText);
 
   const links = await page.evaluate(() =>
     Array.from(document.querySelectorAll("#courses .card"), (e) => ({}))
