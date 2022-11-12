cf12dbeeaeafc8e929ad59f37661ebfff7ea72bd
Danish Saleem <danish_s@outlook.com>
2022-11-12 15:41:38 +0500

    Setup Project files

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+async function run() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://traversymedia.com");
+
+  await page.screenshot({ path: "example.png", fullPage: true });
+  await page.pdf({ path: "example.pdf", format: "A4" });
+
+  await browser.close();
+}
+
+run();
